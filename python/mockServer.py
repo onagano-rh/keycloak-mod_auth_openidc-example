ed78ae61171e7117addafd08433a2b0cfb350020
NAGANO Osamu <onagano@redhat.com>
2024-01-15 13:18:40 +0900

    Try to use utf-8 but doesn't work as expected

--- python/mockServer.py
+++ python/mockServer.py
@@ -8,13 +8,13 @@ class ServerHandler(s.BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-type', 'text/html; charset=UTF-8')
         self.end_headers()
-        self.wfile.write("{} {} {}<br>\n".format(self.command, self.path, self.request_version).encode())
+        self.wfile.write("{} {} {}<br>\n".format(self.command, self.path, self.request_version).encode('utf-8'))
         for h in self.headers:
-            self.wfile.write("{}: {}<br>\n".format(h, self.headers[h]).encode())
-        self.wfile.write("<a href='http://localhost:8080/app/callback?logout=backchannel'>Logout</a>\n".format().encode())
-        self.wfile.write("<a href='http://localhost:8180/realms/demo/protocol/openid-connect/logout?post_logout_redirect_uri={}&client_id={}&id_token_hint={}'>Logout 2</a>\n".format('http%3A%2F%2Flocalhost%3A8080%2F', 'reverse-proxy-app', self.headers['OIDC_id_token']).encode())
+            self.wfile.write("{}: {}<br>\n".format(h, self.headers[h]).encode('utf-8'))
+        self.wfile.write("<a href='http://localhost:8080/app/callback?logout=backchannel'>Logout</a>\n".format().encode('utf-8'))
+        self.wfile.write("<a href='http://localhost:8180/realms/demo/protocol/openid-connect/logout?post_logout_redirect_uri={}&client_id={}&id_token_hint={}'>Logout 2</a>\n".format('http%3A%2F%2Flocalhost%3A8080%2F', 'reverse-proxy-app', self.headers['OIDC_id_token']).encode('utf-8'))
 
 httpd = s.HTTPServer((LISTEN_ADDRESS, LISTEN_PORT), ServerHandler)
 httpd.serve_forever()
